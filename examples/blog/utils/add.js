521034feb8f8e5a6a713f9e35af44841fedb8d33
Alex Patterson <alex.patterson@fusionauth.io>
2023-11-19 17:56:11 -0500

    10k

---
+++ examples/blog/utils/add.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const copies = 10000;
+
+const file = 'using-mdx';
+const root = fileURLToPath(new URL(`../src/content/blog`, import.meta.url));
+
+for (const i of Object.keys(Array(copies).fill())) {
+	fs.copyFileSync(`${root}/${file}.mdx`, `${root}/${file}-${i}.mdx`)
+}
