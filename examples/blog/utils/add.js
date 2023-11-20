c452e0ea0e4e30ee9bb61448bf90efef1228588f
Alex Patterson <alex.patterson@fusionauth.io>
2023-11-20 05:48:15 -0500

    with 5k

--- examples/blog/utils/add.js
+++ examples/blog/utils/add.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 
-const copies = 1000;
+const copies = 5000;
 
 const file = 'using-mdx';
 const root = fileURLToPath(new URL(`../src/content/blog`, import.meta.url));
